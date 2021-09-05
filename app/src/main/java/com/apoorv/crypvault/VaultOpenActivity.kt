3686609fdb2c5d6cb935e9faff2af6e68e8b8172
Apoorv Verma <sunny.verma81201@gmail.com>
2021-09-05 19:25:51 +0530

    added launcher icon and vault opening activity

---
+++ app/src/main/java/com/apoorv/crypvault/VaultOpenActivity.kt
@@ -0,0 +1,11 @@
+package com.apoorv.crypvault
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VaultOpenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vault_open)
+    }
+}
